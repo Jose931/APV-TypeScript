fbb42b83673705a6bc0303308da6a557e83c5fe2
Jose <jlaresti93@gmail.com>
2024-06-12 00:31:15 +0200

    Proyecto de administrar pacientes cambiado a ts

---
+++ src/selectores.ts
@@ -0,0 +1,10 @@
+// Selectores
+export const pacienteInput = document.querySelector<HTMLInputElement>('#paciente')
+export const propietarioInput = document.querySelector<HTMLInputElement>('#propietario')
+export const emailInput = document.querySelector<HTMLInputElement>('#email')
+export const fechaInput = document.querySelector<HTMLInputElement>('#fecha')
+export const sintomasInput = document.querySelector<HTMLTextAreaElement>('#sintomas')
+
+export const formulario = document.querySelector<HTMLFormElement>('#formulario-cita')
+export const formularioInput = document.querySelector<HTMLInputElement>('#formulario-cita input[type="submit"]')
+export const contenedorCitas = document.querySelector<HTMLDivElement>('#citas')
